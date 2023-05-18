9d9f40813abb6ee22279242497395d215db31c2f
Efren Galarza <efrenpgc2602@gmail.com>
2023-05-18 15:47:12 -0500

    update Documentacion'

--- semver.sh
+++ semver.sh
@@ -3,27 +3,70 @@
 # Autor: efrxngg
 # Descripción:
 # Script para automatizar el proceso de cambio de versión en los archivos del proyecto
-# y manejar diferentes escenarios de uso mediante la interfaz de línea de comandos (CLI).
-
+# y manejar diferentes ambientes a través de una interfaz de línea de comandos (CLI).
+# Se basa en la convención SemVer, que consta de tres partes: major (Mayor), minor (Menor) y patch (Parche).
+# Cada parte tiene un propósito específico y reglas para su modificación (Semantic Versioning).
+# Además, permite el cambio en los archivos de configuración para el cambio de entorno: prod (Producción) e inc (Incubadora).
+#
 # [Requisitos]
 # Marcar las siguientes etiquetas con un comentario identificador
-# -pom.xml: 
-#   project/version: (EJEMPLO) 
-#       </version> <!-- idVersionProject -->
-#   project/artifactId: (EJEMPLO) 
-#       </artifactId> <!-- idArtifactProject -->
-#   project/properties/openshiftProjectName: (EJEMPLO)
-#       </openshiftProjectName> <!-- idNameOpenShiftProjectSelected -->
+# - pom.xml:
+#   - project/version: (EJEMPLO)
+#     </version> <!-- idVersionProject -->
 #
-# -dc.yaml: 
-#   spec.replicas: (EJEMPLO)
-#       replicas: 2 #idNumbReplicasSelected
-#   spec.template.spec.containers.image: (EJEMPLO)
-#       image: docker-registry.default.svc:5000/claro-service-waiver/edx-renuncia-webbff:1.3.3 #idImageSelected
+#   - project/artifactId: (EJEMPLO)
+#     </artifactId> <!-- idArtifactProject -->
 #
-# -route.yml: 
-#   spec.host: (EJEMPLO)
-#       host: edx-renuncia-webbff.openshift-apps.conecel.com #idHostSelected
+#   - project/properties/openshiftProjectName: (EJEMPLO)
+#     </openshiftProjectName> <!-- idNameOpenShiftProjectSelected -->
+#
+# - dc.yaml:
+#   - spec.replicas: (EJEMPLO)
+#     replicas: 2 #idNumbReplicasSelected
+#
+#   - spec.template.spec.containers.image: (EJEMPLO)
+#     image: docker-registry.default.svc:5000/claro-service-waiver/edx-renuncia-webbff:1.3.3 #idImageSelected
+#
+# - route.yml:
+#   - spec.host: (EJEMPLO)
+#     host: edx-renuncia-webbff.openshift-apps.conecel.com #idHostSelected
+#
+# [Uso]
+# Para utilizar el script, debe ubicarlo en la ruta base de su proyecto.
+# Ejecute el script proporcionando las opciones y argumentos correspondientes.
+#
+# Opciones disponibles:
+# -t <tipo>:
+#    Especifica el tipo de cambio de versión a realizar en el proyecto y sus configuraciones, que puede ser:
+#    - major (Mayor): Modifica X.0.0
+#      - Para cambios incompatibles,
+#      - eliminación de funcionalidades existentes,
+#      - cambios en la estructura de la base de datos que afectan la compatibilidad hacia atrás,
+#      - modificaciones significativas en la forma en que se utilizan o interactúan los componentes principales.
+#    - minor (Menor): Modifica 0.X.0
+#      - Para nuevas funcionalidades,
+#      - mejoras en las funcionalidades existentes sin romper la compatibilidad,
+#      - expansión de la API existente de forma compatible.
+#    - patch (Parche): Modifica 0.0.X
+#      - Para solución de errores conocidos,
+#      - parcheo de vulnerabilidades de seguridad,
+#      - pequeñas mejoras de rendimiento.
+#
+# -e <entorno>: Especifica el ambiente al cual se desea cambiar.
+#    - prod: cambia las rutas, el deployment config y el pom para el pase a producción.
+#    - inc: cambia las rutas, el deployment config y el pom para el pase a incubadora.
+#
+# Ejemplo de uso:
+# ./script.sh -t patch -e prod
+#
+# [Notas]
+# - Las banderas se pueden colocar independientemente una de la otra:
+#   - ./script.sh -t patch
+#   - ./script.sh -e prod
+# - En caso de que sea necesario, se puede modificar la #ruta del proyecto
+#   para indicar la ruta base del proyecto en el que se desea realizar la modificación.
+
+# [Fin]
 
 # Variables del script [Modificable]
 # [Produccion]
@@ -36,8 +79,8 @@ route_incubadora="incubadora-edx-renuncia-webbff.openshift-apps.conecel.com"
 number_replicas_inc=1
 
 # Constantes del Script [UnModifiable]
-path="./retentionprocesses" #ruta del proyecto
-path_info="$path/pom.xml"                #ruta del pom
+path="."                  #ruta del proyecto
+path_info="$path/pom.xml" #ruta del pom
 
 idVersion="<!-- idVersionProject -->"                     #identificador de la version del proyecto
 idArtifact="<!-- idArtifactProject -->"                   #identificador de artifact del proyecto
@@ -55,7 +98,6 @@ type=""        #tipo de cambio de version: major, minor 0 patch
 environment="" #tipo de entorno: prod, inc
 
 # Version Functions
-
 getPomInformation() {
     local patternVersion="<version>([^<]+)<\/version> $idVersion"         #Para que funcione se le debe añadir ese comentario
     local patternArtifact="<artifactId>([^<]+)<\/artifactId> $idArtifact" #Para que funcione se le debe añadir ese comentario
@@ -205,7 +247,7 @@ modifyEnvironment() {
     esac
 }
 
-# Call Function 
+# Call Function
 inputArgs() {
     while getopts "t:e:" opt; do
         case $opt in
